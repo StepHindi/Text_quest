aad9ce7cf35cd7fda13eec7e938f0189f811a022
Hindi_Estebel <fluffycat13@yandex.ru>
2021-12-10 18:07:18 +0300

    ini Location, Action, View

--- src/com/company/Action.java
+++ src/com/company/Action.java
@@ -1,4 +1,14 @@
 package com.company;
 
 public class Action {
+    String text;
+    int choice;
+
+    String getText() {
+        return text;
+    }
+
+    int getDestLocationId() {
+        return choice;
+    }
 }
--- src/com/company/Location.java
+++ src/com/company/Location.java
@@ -1,5 +1,19 @@
 package com.company;
 
 public class Location {
+    String text;
+    Action[] actions;
 
+    Location (String text, Action[] actions) {
+        this.text = text;
+        this.actions = actions;
+    }
+
+    String getText() {
+        return text;
+    }
+
+    Action[] getActions() {
+        return actions;
+    }
 }
