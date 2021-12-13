b9489babf6c4927249eef3cac6c21b2f2a5f1347
Hindi <stephindisth@gmail.com>
2021-12-13 07:43:00 +0300

    upd 2

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,39 @@
 package com.company;
 
-public class Main {
+import java.util.Scanner;
+
+public class Main implements UserChoiseListener{
 
     public static void main(String[] args) {
-	// write your code here
+        int locId = 0;
+        boolean corr_answer = false;
+        Scanner sc = new Scanner(System.in);
+        View screen = new View();
+        Storage storage = new Storage();
+        screen.showLocation(storage.getLocation(locId));
+        while (true) {
+            while (true) {
+                String answer = sc.nextLine();
+                for (Action el : storage.getLocation(locId).actions) {
+                    if (el.text.equals(answer)) {
+                        locId = el.choice;
+                        corr_answer = true;
+                        break;
+                    }
+                }
+                if (!corr_answer) {
+                    System.out.println("wrong answer!");
+                } else {
+                    break;
+                }
+            }
+            screen.showLocation(storage.getLocation(locId));
+            corr_answer = false;
+        }
+    }
+
+    @Override
+    public void userChoice() {
+
     }
 }
--- src/com/company/View.java
+++ src/com/company/View.java
@@ -2,7 +2,11 @@ package com.company;
 
 public class View {
     void showLocation(Location loc) {
-        System.out.print(loc.text);
+        System.out.print(loc.text + " Для выбора введите: ");
+        for (Action el : loc.actions) {
+            System.out.print(el.text + " ");
+        }
+        System.out.println();
 
     }
 }
